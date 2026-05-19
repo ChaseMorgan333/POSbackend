481a125d2a09b6c02c79c48f2d559be3589aaed4
ChaseMorgan333 <93793187+ChaseMorgan333@users.noreply.github.com>
2026-05-19 17:44:38 -0500

    Project Initialized with Spring Initializr Language: Java Java Version: 19 Build Tool: Maven Spring Boot Version: 3.5.14

---
+++ restaurant-pos-backend/src/main/java/com/chasemorgan/restaurantposbackend/employee/Employee.java
@@ -0,0 +1,4 @@
+package com.chasemorgan.restaurantposbackend.employee;
+
+public class Employee {
+}
---
+++ restaurant-pos-backend/src/test/java/com/chasemorgan/restaurantposbackend/RestaurantPosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chasemorgan.restaurantposbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantPosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
